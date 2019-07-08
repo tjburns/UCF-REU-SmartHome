38a9049d1c5cc839762a2f1f578543636018a57d
tjburns <tjburns28@gmail.com>
2019-07-08 14:19:08 -0400

    modified version for testing

--- sequences.py
+++ sequences.py
@@ -6,18 +6,28 @@ import random
 def sequence_random():
 	#TODO easiest sequence
 		#randomize events
-		
-	num_ops = random.randint(1,6)
-	for _ in range(num_ops):
-		random_motor = random.randint(0,16)
-		if state[random_motor] == 'closed':
-			motorOpen(random_motor)
-			state[random_motor] = 'open'
-			print(motor_names[random_motor] + ' opened')
-		elif state[random_motor] == 'open':
-			motorClose(random_motor)
-			state[random_motor] = 'closed'
-			print(motor_names[random_motor] + ' closed')
+	
+	try:
+		while True:
+			num_ops = random.randint(1,6)
+			for _ in range(num_ops):
+				random_motor = random.randint(0,16)
+				if state[random_motor] == 'closed':
+					motorOpen(random_motor)
+					state[random_motor] = 'open'
+					print(motor_names[random_motor] + ' opened')
+				elif state[random_motor] == 'open':
+					motorClose(random_motor)
+					state[random_motor] = 'closed'
+					print(motor_names[random_motor] + ' closed')
+					
+			for i in range(0,300):
+				print('Next action in: ' + str(i)+'/300', flush=True)
+				time.sleep(1)
+	except KeyboardInterrupt:
+		print("Keyboard interrupt.")
+	except Exception as e: 
+		print(e)
 
 def sequence_NormalDay():
 	#TODO write method simulating a normal day:
